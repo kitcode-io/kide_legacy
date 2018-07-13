a7c5a2a0de95282713c8c440350868c0441411e6
satnam-sandhu <singhsandhusatnam@outlook.com>
2018-07-13 17:49:32 +0530

    updated font awesome cdn and included event based dom injection

--- server.js
+++ server.js
@@ -3,34 +3,33 @@ var Config = require("./config.json");
 
 
 var manifest = {
-    connections: [{
-        port: Config.server.web.port,
-        labels: ['web'],
-    }],
-    registrations: [{
-            plugin: {
-                register: './servers/web',
-                options: {
-                    config: Config
-                }
-            },
-            options: {
-                select: ['web']
-            }
-        }
-    ]
+  connections: [{
+    port: Config.server.web.port,
+    labels: ['web'],
+  }],
+  registrations: [{
+    plugin: {
+      register: './servers/web',
+      options: {
+        config: Config
+      }
+    },
+    options: {
+      select: ['web']
+    }
+  }]
 };
 
 var options = {
-    relativeTo: __dirname,
+  relativeTo: __dirname,
 };
 
 Glue.compose(manifest, options, function(err, server) {
-    if (err) {
-        throw err;
-    }
-
-    server.start(function() {
-        console.log('Hapi days!');
-    });
+  if (err) {
+    throw err;
+  }
+  server.start(function(err) {
+    if (err) throw err;
+    console.log('Hapi days!');
+  });
 });
