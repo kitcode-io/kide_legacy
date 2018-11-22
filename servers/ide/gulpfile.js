27eff6109f4f612efdb21dd44e10cbac61b9859e
bonnie <bonnie@bonnies-MacBook-Air.local>
2018-11-22 22:39:47 +0530

    package versions updated

--- servers/ide/gulpfile.js
+++ servers/ide/gulpfile.js
@@ -76,82 +76,9 @@ Gulp.task("editor:inject",["editor:styles:build","editor:scripts:build"], functi
 });
 
 Gulp.task("editor:watch", function () {
-  Sequence([ "editor:styles:build", "editor:scripts:build" ]);
-  
+  Sequence([ "editor:styles:build", "editor:scripts:build" ]); 
   Gulp.watch([ "public/editor/**/*.css", "public/editor/**/*.js" ], [ "editor:inject" ]);
-
   Gulp.watch([ "assets/css/**/*.css", "assets/css/**/*.less" ], [ "editor:styles:build" ]);
 });
 
-
-////////////////////////////////////////////////////////////////////////////////
-
-Gulp.task("landing:styles:clean", function () {
-  return Gulp.src([ "public/landing/*.css" ], { read: true })
-    .pipe(Rimraf());
-});
-
-Gulp.task("landing:scripts:clean", function () {
-  return Gulp.src([ "public/landing/*.js" ], { read: true })
-    .pipe(Rimraf());
-});
-
-Gulp.task("landing:styles:build", [ "landing:styles:clean" ], function () {
-  return Gulp.src([ "assets/css/apps/landing.less" ])
-    .pipe(Less({
-      paths: [ ".", __dirname + "assets/css" ]
-    }))
-     .pipe(Rev())
-    .pipe(Gulp.dest("public/landing"));
-});
-
-Gulp.task("landing:scripts:build", [ "landing:scripts:clean" ], function () {
-  var bundler = browserify({
-    entries: [
-      "./assets/js/apps/landing.js"
-    ]
-  });
-  
-  bundler.transform("coffeeify");
-  bundler.transform("brfs");
-  
-  bundler.on("update", rebundle);
-  
-  return rebundle();
-  
-  function rebundle () {
-    return bundler.bundle()
-      .on("error", function (e) {
-        Gutil.log("Bundling error", e.message);
-      })
-      .pipe(Source("landing.js"))
-      .pipe(Rev())
-      .pipe(Gulp.dest("public/landing"));
-  }
-});
-
-Gulp.task("landing:inject",[ "landing:styles:build","landing:scripts:build" ], function () {
-  return Gulp.src("views/landing.html")
-    .pipe(Inject(Gulp.src([ "public/landing/*.js", "public/landing/*.css" ], { read: false }), {
-      ignorePath: "/public",
-      addRootSlash: true
-    }))
-    .pipe(Inject(Gulp.src("public/config.json"), {
-      transform: function (filepath, file) {
-        return '<script>angular.module("plunker.service.config", []).value("config",JSON.parse(atob("' + file.contents.toString("base64") + '")));</script>';
-      }
-    }))
-    .pipe(Gulp.dest("views"));
-});
-
-Gulp.task("landing:watch", function () {
-  Sequence([ "landing:styles:build", "landing:scripts:build" ]);
-  
-  Gulp.watch([ "public/landing/**/*.css", "public/landing/**/*.js" ], [ "landing:inject" ]);
-
-  Gulp.watch([ "assets/css/**/*.css", "assets/css/**/*.less" ], [ "landing:styles:build" ]);
-});
-
-////////////////////////////////////////////////////////////////////////////////
-Gulp.task("default", ["editor:watch", "landing:watch"]);
 Gulp.task("build",[ "editor:inject"]);
